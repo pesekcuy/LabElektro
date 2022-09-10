f8553de862107d7d73133459c5eecea7807dd7b0
Adi Nugroho <adi.nugroho@tutanota.com>
2022-09-10 19:54:34 +0700

    Update allowed hosts

--- LabElektro/settings.py
+++ LabElektro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', '192.168.1.107']
 
 
 # Application definition
